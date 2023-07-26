e7c62121b95c5113fb98bdb99cc808460ace64fa
박용진 <insight.emoket@gmail.com>
2023-07-26 23:31:09 +0900

    projects[] 항목 주석 처리

--- site.config.js
+++ site.config.js
@@ -10,12 +10,12 @@ const CONFIG = {
     github: "emoket",
     instagram: "",
   },
-  projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
-    },
-  ],
+  // projects: [
+  //   {
+  //     name: `morethan-log`,
+  //     href: "https://github.com/morethanmin/morethan-log",
+  //   },
+  // ],
   // blog setting (required)
   blog: {
     title: "L_earn",
@@ -27,7 +27,6 @@ const CONFIG = {
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
-
 
   // notion configuration (required)
   notionConfig: {
