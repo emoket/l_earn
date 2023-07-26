74afe5d2d7f70547ce4f3884b56c8b515f257bba
박용진 <insight.emoket@gmail.com>
2023-07-26 22:47:48 +0900

    site configured

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "emoket",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Frontend Engineer",
+    bio: "Learn Share and Enjoy",
+    email: "insight.emoket@gmail.com",
+    linkedin: "emoket",
+    github: "emoket",
     instagram: "",
   },
   projects: [
@@ -18,15 +18,16 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "L_earn",
+    description: "Learn Share and Enjoy",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://blog.emoket.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+
 
   // notion configuration (required)
   notionConfig: {
@@ -47,9 +48,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "emoket/l_earn",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
