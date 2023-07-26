616753968c5769b08275dda78614e051c4fff798
박용진 <insight.emoket@gmail.com>
2023-07-26 23:37:31 +0900

    projects 값을 빈 배열로 (항목 자체를 삭제하지 않고)

--- site.config.js
+++ site.config.js
@@ -10,12 +10,12 @@ const CONFIG = {
     github: "emoket",
     instagram: "",
   },
-  // projects: [
-  //   {
-  //     name: `morethan-log`,
-  //     href: "https://github.com/morethanmin/morethan-log",
-  //   },
-  // ],
+  projects: [
+    //   {
+    //     name: `morethan-log`,
+    //     href: "https://github.com/morethanmin/morethan-log",
+    //   },
+  ],
   // blog setting (required)
   blog: {
     title: "L_earn",
